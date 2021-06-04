5666f9068e315704b7d1030dedb108a81b915964
Brenden Ferguson <brenden.ferguson@gmail.com>
2021-06-04 15:48:58 -0700

    adds interval through args

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
 use cpufreq;
 use std::{thread, time};
+use std::env;
 
 fn main() {
-    let one_second = time::Duration::from_secs(1);
+
+    let args: Vec<String> = env::args().collect();
+    
+    let mut interval = time::Duration::from_millis(1000);
+
+    match args.len(){
+        2=>{
+            interval = time::Duration::from_millis(args[1].parse().unwrap());
+        },
+        _=>{
+            eprintln!("Error, usage: 'cpufreq 500'. Defaulting to 1000ms refresh.");
+            thread::sleep(interval)
+        }
+    }
+   
+    
     loop {
         let data = cpufreq::get_data();
 
@@ -11,7 +27,7 @@ fn main() {
         let formatted_results = cpufreq::format(results);
 
         println!("{:?}", formatted_results);
-        
-        thread::sleep(one_second);
+
+        thread::sleep(interval);
     }
 }
